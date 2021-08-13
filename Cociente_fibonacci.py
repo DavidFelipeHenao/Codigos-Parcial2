fd9e9ffd2b1095b73b4735d1fd4d850ff6887550
DavidFelipeHenao <dfelipe.henao@udea.edu.co>
2021-08-13 09:18:18 -0500

    Nueva version mejorada del codigo

--- Cociente_fibonacci.py
+++ Cociente_fibonacci.py
@@ -2,18 +2,37 @@
 # coding: utf-8
 
 # In[45]:
+'''
+Este codigo crea una funcion que obtiene los terminos de la sucesion de Fibonacci
+y que devuelve el cociente entre los dos ultimos terminos consecutivos para obtener una
+aproximacion a la razon aurea.
 
+Parametros de entrada: 
+    n: Numero de razones a calcular
+
+Autor: David Felipe Henao
+Ultima actualizacion: 13 agosto, 2021    
+'''
 
 def fib(n):
-    a = 0
-    b = 1
+    
+    rAurea =  1.618033988
+
+    # Numeros para iniciar la serie
+    f0 = 0
+    f1 = 1
     
     for k in range(1, n+1):
-        c = b+a
-        a = b
-        b = c
-    print(b/a) 
-fib(5)
+        fn = f1+f0
+        
+        # Actualiza los numeros
+        f0 = f1
+        f1 = fn
+    
+    # Imprime el cociente entre los dos ultimos numeros consecutivos
+    print(f"El cociente obtenido es {f1/f0}")
+    print(f"La diferencia respecto a la razon aurea es {abs((f1/f0)-rAurea)}") 
+fib(10)
     
 
 
--- fibonacci.py
+++ fibonacci.py
@@ -2,25 +2,42 @@
 # coding: utf-8
 
 # In[22]:
+'''
+Este codigo imprime en pantalla los n primeros terminos de la sucesion de
+Fiibonacci.
 
+Parametros de entrada:
+    n: cantidad de terminos a generar.
+
+Autor: DAvid Felipe Henao
+Ultima actualizaicion: 13 agosto, 2021
+'''
 
 n = int(input("Ingrese cuantos numeros de fibonacci quiere generar: "))
-a=1
-b=1
-j=1
+
+# Numeros para iniciar la serie
+f1=1
+f2=1
+
 if n == 1:
     print('0')
 elif n == 2:
     print('0','1')
 else:
+    
+    # Imprime los numeros que inician la serie
     print('0')
-    print(a)
-    print(b)
+    print(f1)
+    print(f2)
     for i in range(n-3):
-        total = a + b
-        b =a
-        a = total
-        print(total)
+        fn = f1 + f2
+        
+        # Actualiza los numeros 
+        f2 = f1
+        f1 = fn
+        print(fn)
+
+
 
 
 
